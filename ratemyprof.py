7bd648010c1f41f849ac64a818c2fe78a1963b5e
ldongusf <ldong7@usfca.edu>
2015-09-03 14:46:31 -0700

    json

--- ratemyprof.py
+++ ratemyprof.py
@@ -4,48 +4,50 @@ test comment
 
 import json
 import requests
-import ssl
 from bs4 import BeautifulSoup
 
+AMOUNT = 9
+# f = open('draft', 'w')
 
-amount = 9
-f = open('draft', 'w')
+# count = 0
+COMMENTS = []
+for i in range(1, AMOUNT):
+    request = requests.get("http://www.ratemyprofessors.com/campusrating/pagi" +
+                           "natecampusRatings?page=" + str(i) + "&sid=1600")
 
-count = 0
-
-for i in range(1, amount):
-    request = requests.get("http://www.ratemyprofessors.com/campusrating/paginatecampusRatings?page="
-                           + str(i) + "&sid=1600")
-
-
-   
-
-    data = BeautifulSoup(request.text, "html.parser")
-
-    comments = data.get_text()
-
-    
-    for c in comments:
-        f.write(c)
+    dic = request.json()
+    for entry in dic['ratings']:
+        COMMENTS.append(entry['crComments'])
 
 
-f.close()
+for comment in COMMENTS:
+    print comment
+    print '\n'
 
-d = open('draft', 'r')
-r = open('rate', 'w')
-for line in d:
-    if "crComments" in line:
-        count += 1
-        for word in line.split():
-            word1 = word.replace('\"', '')
-            word2 = word1.replace("crComments", '')
-            word3 = word2.replace(":", '')
-            r.write(word3 + " ")
-        r.write("\n\n")
+print len(COMMENTS)
+    # data = BeautifulSoup(request.text, "html.parser")
+
+#     comments = data.get_text()
+
+#     for c in comments:
+#         f.write(c)
+
+# f.close()
+
+# d = open('draft', 'r')
+# r = open('rate', 'w')
+# for line in d:
+#     if "crComments" in line:
+#         count += 1
+#         for word in line.split():
+#             word1 = word.replace('\"', '')
+#             word2 = word1.replace("crComments", '')
+#             word3 = word2.replace(":", '')
+#             r.write(word3 + " ")
+#         r.write("\n\n")
 
 
-r.write("count: " + str(count))
-d.close()
-r.close()
-    
-         
+# r.write("count: " + str(count))
+# d.close()
+# r.close()
+#     
