aa1df366f5d3a25b469a0d5b0997a9e3fef5cf18
ldongusf <ldong7@usfca.edu>
2015-09-03 15:01:22 -0700

    clean up

--- yelp.py
+++ yelp.py
@@ -1,26 +1,29 @@
+"""
+scrap usf related comments from Yelp
+"""
 import requests
 from bs4 import BeautifulSoup
 
 TOTAL = 80
 INCREMENT = 40
 
-f = open('yelp', 'w')
+FILE = open('yelp', 'w')
 
 i = 0
 count = 0
 
 while i <= TOTAL:
-    request = requests.get("http://www.yelp.com/biz/university-of-san-francisco"
-                           +"-san-francisco?start=" + str(i))
+    request = requests.get("http://www.yelp.com/biz/university-of-san-franci" +
+                           "sco-san-francisco?start=" + str(i))
     data = BeautifulSoup(request.text, "html.parser")
-    for review in data.find_all("p", {"itemprop" : "description"}):
+    for review in data.find_all("p", {"itemprop": "description"}):
         count += 1
-        f.write(str(count) + ".")
-        f.write(review.get_text().encode('utf-8'))
-        f.write("\n\n")
+        FILE.write(str(count) + ".")
+        FILE.write(review.get_text().encode('utf-8'))
+        FILE.write("\n\n")
 
-    i += INCREMEMENT
+    i += INCREMENT
     print i
 
-f.write("The count is:" + str(count))
-f.close()
+FILE.write("The count is:" + str(count))
+FILE.close()
