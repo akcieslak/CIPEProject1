db5d9f54aae5338da3d33621cb70b925c67a4c0d
ldongusf <ldong7@usfca.edu>
2015-09-03 16:03:00 -0700

    clean up

--- gradreports.py
+++ gradreports.py
@@ -2,23 +2,22 @@
 scrape usf related comments from gradereports.com
 """
 import requests
-import re
 from bs4 import BeautifulSoup
 
 count = 0
 FILE = open('gradreport', 'w')
 
-request = requests.get("http://www.gradreports.com/colleges/university-of-san-" +
-                       "francisco")
+request = requests.get("http://www.gradreports.com/colleges/university-of-sa" +
+                       "n-francisco")
 
 data = BeautifulSoup(request.text, "html.parser")
 
-for review in data.find_all("div", {"class" : "review_0"}):
+for review in data.find_all("div", {"class": "review_0"}):
     count += 1
     FILE.write(str(count) + ". ")
     FILE.write(review.get_text())
     FILE.write("\n\n")
 
-            
+
 FILE.write("The count is: " + str(count))
 FILE.close()
--- studentreview.py
+++ studentreview.py
@@ -1,7 +1,10 @@
+"""
+scrape usf related comments from studentsreview.com
+"""
 import requests
 import re
-
 from bs4 import BeautifulSoup
+
 AMOUNT = 5
 MAJORS = []
 count = 0
@@ -9,14 +12,15 @@ count = 0
 
 FILE = open('studentR', 'w')
 
-##Goes through the root site and pulls down the html
+# Goes through the root site and pulls down the html
 for i in range(1, AMOUNT):
-    request = requests.get("http://www.studentsreview.com/gettingin.php3?SH=USF&ST="
-                    +"CA&page=" + str(i) +"&d_school=University%20of%20San%20" +
-                       "Francisco&specific=grade_academic_gpa")
+    request = requests.get("http://www.studentsreview.com/gettingin.php3?SH=" +
+                           "USF&ST=CA&page=" + str(i) + "&d_school=Universit" +
+                           "y%20of%20San%20Francisco&specific=grade_academic" +
+                           "_gpa")
     data = BeautifulSoup(request.text, "html.parser")
 
-    ##Finds all the links that have d_name=University of San Francisco"
+    # Finds all the links that have d_name=University of San Francisco"
     for href in data.find_all('a'):
         if "d_name=University of San Francisco" in str(href):
             link = href['href']
@@ -25,32 +29,33 @@ for i in range(1, AMOUNT):
                 break
             else:
                 MAJORS.append(link)
-            
+
             pageRequest = requests.get(link)
             pageData = BeautifulSoup(pageRequest.text, "html.parser")
-            
+
             for commentHref in pageData.find_all('a'):
                 if "/viewprofile" in str(commentHref):
                     commentPage = commentHref['href']
-                    commentRequest = requests.get("http://www.studentsreview.com/" + commentPage)
-                    commentData = BeautifulSoup(commentRequest.text, "html.parser")
-                    
-                    #info is each table
-                    for info in commentData.findAll("div", {"class" : "leftColumn"}):
+                    commentRequest = requests.get("http://www.studentsreview" +
+                                                  ".com/" + commentPage)
+                    commentData = BeautifulSoup(commentRequest.text,
+                                                "html.parser")
 
-                        for review in info.find_all("div",{"class" : "reviewcomment"}):
+                    # info is each table
+                    for info in commentData.findAll("div", {"class": "leftCo" +
+                                                            "lumn"}):
+
+                        for review in info.find_all("div", {"class": "review" +
+                                                            "comment"}):
                             count += 1
                             FILE.write(str(count) + ". ")
-                            regex = re.compile("(Major: )([\w+\s\W*]*)(\(This Major's Salary over time\))")
+                            regex = re.compile("(Major: )([\w+\s\W*]*)(\(Thi" +
+                                               "s Major's Salary over time\))")
                             edited = regex.sub('', review.get_text())
-                                                                         
+
                             FILE.write(edited)
                             FILE.write("\n\n")
 
-                       
+
 FILE.write("The count is: " + str(count))
-FILE.close()                          
-                                                
-
-    
-
+FILE.close()
