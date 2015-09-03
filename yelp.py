97f698db2ef911a8568be1be5dfef5eee1f57a47
ldongusf <ldong7@usfca.edu>
2015-09-03 14:07:46 -0700

    unicode fix

--- yelp.py
+++ yelp.py
@@ -11,15 +11,17 @@ while i <= 80:
                            +"-san-francisco?start=" + str(i))
     data = BeautifulSoup(request.text, "html.parser")
     for review in data.find_all("p", {"itemprop" : "description"}):
-        try:
-            count += 1
-            f.write(str(count) + ".")
-            f.write(review.get_text())
-            f.write("\n\n")
-        except UnicodeEncodeError:
-            f.write("\n\n")
-            print "There was a UnicodeEncodeError"
-            pass
+        # try:
+        count += 1
+        f.write(str(count) + ".")
+        f.write(review.get_text().encode('utf-8'))
+        f.write("\n\n")
+        # except UnicodeEncodeError, e:
+        #     z = e
+        #     f.write("\n\n")
+        #     print "There was a UnicodeEncodeError"
+        #     print z
+        #     pass
 
     i += 40
     print i
