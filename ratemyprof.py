c3c61c68a1fdccff28e03cd205b3894969b47d1e
akcieslak <akcieslak@dons.usfca.edu>
2015-09-03 14:26:54 -0700

    updated stuff

--- ratemyprof.py
+++ ratemyprof.py
@@ -1,19 +1,15 @@
-"""
-test comment
-"""
-
-import json
 import requests
-import ssl
 from bs4 import BeautifulSoup
 
 
-amount = 9
-f = open('draft', 'w')
-
+TOTAL = 9
+TAG = "crComments"
+temp = []
 count = 0
+r = open('rate', 'w')
 
-for i in range(1, amount):
+
+for i in range(1, TOTAL):
     request = requests.get("http://www.ratemyprofessors.com/campusrating/paginatecampusRatings?page="
                            + str(i) + "&sid=1600")
 
@@ -26,26 +22,20 @@ for i in range(1, amount):
 
     
     for c in comments:
-        f.write(c)
+        temp.append(c)
 
 
-f.close()
 
-d = open('draft', 'r')
-r = open('rate', 'w')
-for line in d:
-    if "crComments" in line:
+for line in temp:
+    if TAG in line:
         count += 1
         for word in line.split():
-            word1 = word.replace('\"', '')
-            word2 = word1.replace("crComments", '')
-            word3 = word2.replace(":", '')
-            r.write(word3 + " ")
+            word1 = word.replace('\"', '').replace(TAG, '').replace(":", '')
+            r.write(word1 + " ")
         r.write("\n\n")
 
 
 r.write("count: " + str(count))
-d.close()
 r.close()
     
          
--- yelp.py
+++ yelp.py
@@ -1,29 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
+TOTAL = 80
+INCREMENT = 40
+
 f = open('yelp', 'w')
 
 i = 0
-
 count = 0
-while i <= 80:
+
+while i <= TOTAL:
     request = requests.get("http://www.yelp.com/biz/university-of-san-francisco"
                            +"-san-francisco?start=" + str(i))
     data = BeautifulSoup(request.text, "html.parser")
     for review in data.find_all("p", {"itemprop" : "description"}):
-        # try:
         count += 1
         f.write(str(count) + ".")
         f.write(review.get_text().encode('utf-8'))
         f.write("\n\n")
-        # except UnicodeEncodeError, e:
-        #     z = e
-        #     f.write("\n\n")
-        #     print "There was a UnicodeEncodeError"
-        #     print z
-        #     pass
 
-    i += 40
+    i += INCREMEMENT
     print i
 
 f.write("The count is:" + str(count))
